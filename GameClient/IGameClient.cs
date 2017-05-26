32b91c91feece21f35bc3e130d1432ee886cb417
robinschiro <robinschiro@gmail.com>
2017-05-26 10:46:06 -0400

    MD #24: Removed all references to and usages of the 'selection' feature. (#40)
    Signed-off-by: Robin Schiro <robinschiro@gmail.com>

--- GameClient/IGameClient.cs
+++ GameClient/IGameClient.cs
@@ -15,14 +15,11 @@ namespace GameClient
         void DisplayOpponentCards( Object filler = null );
         void AddCardToGrid( Card card, Grid grid, Player player, bool isHand, int position );
         //void RemoveCardButtonFromHand( Button cardButton );
-        void SelectCard( Button cardButton );
-        void DeselectCard( Button cardButton );
 
         #endregion
 
         #region Events
 
-        void SelectCardEvent( object sender, MouseButtonEventArgs args );
         void PlayCardEvent( object sender, MouseButtonEventArgs args );
 
         #endregion
