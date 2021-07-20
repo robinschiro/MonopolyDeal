b25679772edb46fa0583401ba6fa1a6dcf402861
Robin <robinschiro@gmail.com>
2021-07-19 18:34:59 -0700

    Add dummy project to enable two client windows to open when debugging

---
+++ GameClientDummy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameClientDummy
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
