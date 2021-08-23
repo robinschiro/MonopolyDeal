9c4292ee472295e6b145334088df5397a4a9fe7a
Robin <robinschiro@gmail.com>
2021-08-22 17:46:02 -0700

    Include comment to explain need for Program class in dummy project

--- SecondGameClient/Program.cs
+++ SecondGameClient/Program.cs
@@ -1,5 +1,8 @@
 ﻿namespace SecondGameClient
 {
+    /// <summary>
+    /// Although this project is never built, an entrypoint file is still required for the solution build to succeed on AppVeyor.
+    /// </summary>
     class Program
     {
         static void Main( string[] args )
